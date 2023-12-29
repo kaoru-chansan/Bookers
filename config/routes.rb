5f199f56481cac9496eaecaf6bfe25a73d58941e
EC2 Default User <151661528+kaoru-chansan@users.noreply.github.com>
2023-12-29 17:44:15 +0000

    [一時対応完了]showページの作成、Backリンク、indexページの3個リンク作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  get 'books/new'
-  post 'books' => 'books#create'
-  get 'books' => 'books#index'
-  get 'books/show'
-  get 'books/edit'
-  get '/top' => 'homes#top'
-  git
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+get 'books/new'
+post 'books' => 'books#create'
+root 'books#index'
+get 'books/show'
+get 'books/edit'
+get '/top' => 'homes#top'
+get 'books/:id' => 'books#show', as: 'book'
+get 'books/:id/Back' => 'books#index', as: 'back'
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
