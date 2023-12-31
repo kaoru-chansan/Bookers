a30f3ea5f955eb18aa2c0b0898a986eb6d7f7a00
EC2 Default User <151661528+kaoru-chansan@users.noreply.github.com>
2023-12-31 02:53:17 +0000

    bookのupdate機能の作成(途中)

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -21,7 +21,11 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
-
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+    
 
   private
   #ストロングパラメータ
